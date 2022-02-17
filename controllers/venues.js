16211878b956504e4e0657c960742592bdce6ba0
Nathan Freeman <NathanLFreeman@gmail.com>
2022-02-16 19:56:50 -0500

    Spent all day trying to learn sessions/auth. As a result, only change so far today is to create controllers dir and move routes into it.

---
+++ controllers/venues.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const router = express.Router();
+const Venue = require('../models/venue.js')
+
+//___________________
+// Routes
+//___________________
+//localhost:3000
+// START PAGE ROUTE
+router.get('/' , (req, res) => {
+  Venue.countDocuments({}, (err, count) => {
+    res.render('start.ejs', {
+      tabTitle: 'Venues',
+      numVenues: count
+    });
+  })
+});
+
+// INDEX ROUTE
+router.get('/venues', (req, res) => {
+  Venue.find({}, (err, allVenues) => {
+    Venue.countDocuments({}, (err, count) => {
+      res.render('index.ejs', {
+        tabTitle: 'Venues Home Page',
+        venues: allVenues,
+        venuesCount: count
+      })
+    })
+  })
+})
+
+// NEW ROUTE
+router.get('/venues/new', (req, res) => {
+  res.render('new.ejs', {
+    tabTitle: 'Add New Venue'
+  })
+})
+
+// SHOW ROUTE
+router.get('/venues/:id', (req, res) => {
+  Venue.findById(req.params.id, (err, foundVenue) => {
+    res.render('show.ejs', {
+      tabTitle: foundVenue.name + ' | Details',
+      venue: foundVenue,
+    })
+  })
+})
+
+// CREATE ROUTE
+router.post('/venues', (req, res) => {
+  //reformat form data to match schema
+  req.body.capacity = Number(req.body.capacity)
+  req.body.cost = Number(req.body.cost)
+  req.body.nicknames = req.body.nicknames.split(', ')
+  if(req.body.stillExists === "on") {
+    req.body.stillExists = true 
+  } else {
+    req.body.stillExists = false
+  }
+  req.body.sSports = req.body.sSports.split(', ')
+  req.body.sTeams = req.body.sTeams.split(', ')
+  
+  req.body.links = {} // must initialize 'links' as a key of the data object before you can assign values to it!
+  req.body.links.website = req.body.website
+  req.body.links.twitter = req.body.twitter
+  req.body.links.fb = req.body.fb
+  req.body.links.ig = req.body.ig
+
+  // res.send(req.body)
+  Venue.create(req.body, (err, createdVenue) => {
+    res.redirect('/venues')
+  })
+})
+
+// EDIT ROUTE
+router.get('/venues/:id/edit', (req, res) => {
+  Venue.findById(req.params.id, (err, foundVenue) => {
+    res.render('edit.ejs', {
+      tabTitle: 'Edit ' + foundVenue.name,
+      venue: foundVenue
+    })
+  })
+})
+
+// UPDATE ROUTE
+router.put('/venues/:id', (req, res) => {
+  //reformat form data to match schema
+  req.body.capacity = Number(req.body.capacity)
+  req.body.cost = Number(req.body.cost)
+  req.body.nicknames = req.body.nicknames.split(', ')
+  if(req.body.stillExists === "on") {
+    req.body.stillExists = true 
+  } else {
+    req.body.stillExists = false
+  }
+  req.body.sSports = req.body.sSports.split(', ')
+  req.body.sTeams = req.body.sTeams.split(', ')
+
+  req.body.links = {} // must initialize 'links' as a key of the data object before you can assign values to it!
+  req.body.links.website = req.body.website
+  req.body.links.twitter = req.body.twitter
+  req.body.links.fb = req.body.fb
+  req.body.links.ig = req.body.ig
+  
+  Venue.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, foundVenue) => {
+    res.redirect('/venues')
+  })
+})
+
+router.delete('/venues/:id', (req, res) => {
+  Venue.findByIdAndRemove(req.params.id, (err, droppedVenue) => {
+    res.redirect('/venues')
+  })
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
-const Venue = require('./models/venue.js');
 const app = express ();
+const venuesController = require('./controllers/venues.js')
 const db = mongoose.connection;
 require('dotenv').config()
 
@@ -55,117 +55,9 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+//use venues from controllers folder
+app.use(venuesController)
 
-//___________________
-// Routes
-//___________________
-//localhost:3000
-// START PAGE ROUTE
-app.get('/' , (req, res) => {
-  Venue.countDocuments({}, (err, count) => {
-    res.render('start.ejs', {
-      tabTitle: 'Venues',
-      numVenues: count
-    });
-  })
-});
-
-// INDEX ROUTE
-app.get('/venues', (req, res) => {
-  Venue.find({}, (err, allVenues) => {
-    Venue.countDocuments({}, (err, count) => {
-      res.render('index.ejs', {
-        tabTitle: 'Venues Home Page',
-        venues: allVenues,
-        venuesCount: count
-      })
-    })
-  })
-})
-
-// NEW ROUTE
-app.get('/venues/new', (req, res) => {
-  res.render('new.ejs', {
-    tabTitle: 'Add New Venue'
-  })
-})
-
-// SHOW ROUTE
-app.get('/venues/:id', (req, res) => {
-  Venue.findById(req.params.id, (err, foundVenue) => {
-    res.render('show.ejs', {
-      tabTitle: foundVenue.name + ' | Details',
-      venue: foundVenue,
-    })
-  })
-})
-
-// CREATE ROUTE
-app.post('/venues', (req, res) => {
-  //reformat form data to match schema
-  req.body.capacity = Number(req.body.capacity)
-  req.body.cost = Number(req.body.cost)
-  req.body.nicknames = req.body.nicknames.split(', ')
-  if(req.body.stillExists === "on") {
-    req.body.stillExists = true 
-  } else {
-    req.body.stillExists = false
-  }
-  req.body.sSports = req.body.sSports.split(', ')
-  req.body.sTeams = req.body.sTeams.split(', ')
-  
-  req.body.links = {} // must initialize 'links' as a key of the data object before you can assign values to it!
-  req.body.links.website = req.body.website
-  req.body.links.twitter = req.body.twitter
-  req.body.links.fb = req.body.fb
-  req.body.links.ig = req.body.ig
-
-  // res.send(req.body)
-  Venue.create(req.body, (err, createdVenue) => {
-    res.redirect('/venues')
-  })
-})
-
-// EDIT ROUTE
-app.get('/venues/:id/edit', (req, res) => {
-  Venue.findById(req.params.id, (err, foundVenue) => {
-    res.render('edit.ejs', {
-      tabTitle: 'Edit ' + foundVenue.name,
-      venue: foundVenue
-    })
-  })
-})
-
-// UPDATE ROUTE
-app.put('/venues/:id', (req, res) => {
-  //reformat form data to match schema
-  req.body.capacity = Number(req.body.capacity)
-  req.body.cost = Number(req.body.cost)
-  req.body.nicknames = req.body.nicknames.split(', ')
-  if(req.body.stillExists === "on") {
-    req.body.stillExists = true 
-  } else {
-    req.body.stillExists = false
-  }
-  req.body.sSports = req.body.sSports.split(', ')
-  req.body.sTeams = req.body.sTeams.split(', ')
-
-  req.body.links = {} // must initialize 'links' as a key of the data object before you can assign values to it!
-  req.body.links.website = req.body.website
-  req.body.links.twitter = req.body.twitter
-  req.body.links.fb = req.body.fb
-  req.body.links.ig = req.body.ig
-  
-  Venue.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, foundVenue) => {
-    res.redirect('/venues')
-  })
-})
-
-app.delete('/venues/:id', (req, res) => {
-  Venue.findByIdAndRemove(req.params.id, (err, droppedVenue) => {
-    res.redirect('/venues')
-  })
-})
 
 //___________________
 //Listener
